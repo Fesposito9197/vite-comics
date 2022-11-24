d53d4c584d0bf75af6e71eca8e88d31033393d8b
Fesposito9197 <kekkoesp8@gmail.com>
2022-11-24 17:45:48 +0100

    aggiunge elementi li nav in modo dinamico

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,22 @@
 <script>
 export default {
-    name:'AppHeader'
+    name:'AppHeader',
+    data(){
+        return{
+            links:[
+                'Characters',
+                'Comics',
+                'Movies',
+                'TV',
+                'Games',
+                'Collectibles',
+                'Videos',
+                'Fans',
+                'News',
+                'Shop'
+            ]
+        }
+    }
 }
 </script>
 
@@ -10,16 +26,8 @@ export default {
         <img src="../assets/dc-logo.png" alt="logo">
         <nav>
             <ul>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Comics</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">Games</a></li>
-                <li><a href="#">Collectibles</a></li>
-                <li><a href="#">Videos</a></li>
-                <li><a href="#">Fans</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Shop</a></li>
+                <li v-for="link in links"><a href="#">{{link}}</a></li>
+                
             </ul>
         </nav>
     </div>
